198adb965ee53a9d2963270ec4d70459aa36262b
MSelmanSavas <mehmetselmansavas@gmail.com>
2024-02-03 22:11:57 +0300

    Added entity system components

---
+++ Assets/Scripts/ExternalTool/EntitySystem/EntityDataDictionary.cs
@@ -0,0 +1,7 @@
+using UsefulDataTypes;
+
+[System.Serializable]
+public class EntityComponentDictionary : SerializableReferenceDictionary<TypeReferenceInheritedFrom<EntityComponent_Base>, EntityComponent_Base>
+{
+
+}
