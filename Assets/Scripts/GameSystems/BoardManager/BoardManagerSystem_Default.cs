65d870501ee5350d739e174b6a378355e2417b50
MSelmanSavas <mehmetselmansavas@gmail.com>
2024-02-04 14:31:46 +0300

    Fixed grids are spawned under entity parent. Added grid manager data connection on grids.

--- Assets/Scripts/GameSystems/BoardManager/BoardManagerSystem_Default.cs
+++ Assets/Scripts/GameSystems/BoardManager/BoardManagerSystem_Default.cs
@@ -73,7 +73,7 @@ public class BoardManagerSystem_Default : GameSystem_Base, IGridManager, IEntity
 
         if (Grid.TryGetEntityComponent(out EntityData_GameObject entityData_GameObject))
         {
-            entityData_GameObject.GetGameObject().transform.SetParent(_entityParents);
+            entityData_GameObject.GetGameObject().transform.SetParent(_gridParents);
             entityData_GameObject.GetGameObject().transform.position = GetPosition(index);
         }
 
